cf376c95f397eb87f14255e041670b86abb2bfcd
Winston Kotzan <winston.kotzan@moserit.com>
2025-04-30 15:00:13 -0400

    Backup script improvement

--- db/backup.bash
+++ db/backup.bash
@@ -6,18 +6,27 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Construct the path to ../backups relative to the script location
 BACKUPS_DIR="$SCRIPT_DIR/../backups"
 
-DATE=`date +"%Y-%m-%d-%H%M%S"`
+DATE=$(date +"%Y-%m-%d-%H%M%S")
+ENV_NAME="development"
 
-DB_NAMES=("humhub_mw_production")
+INSTANCE_NAMES=("humhub_mw")
 
-for DB_NAME in "${DB_NAMES[@]}"; do
+for INSTANCE_NAME in "${INSTANCE_NAMES[@]}"; do
+  DB_NAME="${INSTANCE_NAME}_${ENV_NAME}"
 
-  OUT_PATH="${BACKUPS_DIR}/${DB_NAME}-${DATE}.sql.gz"
-  echo "Beginning backup for ${OUT_PATH}"
-  docker compose exec db mariadb-dump --defaults-extra-file=/host/db/db_creds.cnf ${DB_NAME} | gzip -c > $OUT_PATH
+  DB_OUT_PATH="${BACKUPS_DIR}/${DB_NAME}-${DATE}.sql.gz"
+  FILES_OUT_PATH="${BACKUPS_DIR}/${DB_NAME}-files-${DATE}.zip"
 
-  ls -tp ${BACKUPS_DIR}/${DB_NAME}*.sql.gz | grep -v '/$' | tail -n +6 | xargs -I {} rm -- {}
+  echo "Beginning backup for $DB_OUT_PATH"
+  docker compose exec db mariadb-dump --defaults-extra-file=/host/db/db_creds.cnf "$DB_NAME" | gzip -c > "$DB_OUT_PATH"
 
-  /usr/local/bin/aws --profile mhai s3 cp $OUT_PATH s3://mhai-humhub-mw-backups/
+  echo "Beginning backup for $FILES_OUT_PATH"
+  docker compose exec "$INSTANCE_NAME" zip -rq - /var/lib/humhub/ > "$FILES_OUT_PATH"
 
+  /usr/local/bin/aws --profile mhai s3 cp "$DB_OUT_PATH" s3://mhai-humhub-mw-backups/
+  /usr/local/bin/aws --profile mhai s3 cp "$FILES_OUT_PATH" s3://mhai-humhub-mw-files-backups/
+
+  # Clean up old backups
+  ls -tp "${BACKUPS_DIR}/${DB_NAME}"*.sql.gz | grep -v '/$' | tail -n +6 | xargs -I {} rm -- {}
+  ls -tp "${BACKUPS_DIR}/${DB_NAME}"*.zip | grep -v '/$' | tail -n +6 | xargs -I {} rm -- {}
 done
