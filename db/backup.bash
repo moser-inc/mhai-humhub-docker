b581e9f4fde67456ee5c1d745eae2d1744f4fd2a
Winston Kotzan <winston.kotzan@moserit.com>
2025-06-11 16:44:58 -0400

    Tweaks to Backup script output

--- db/backup.bash
+++ db/backup.bash
@@ -15,17 +15,17 @@ for INSTANCE_NAME in "${INSTANCE_NAMES[@]}"; do
   DB_NAME="${INSTANCE_NAME}_${ENV_NAME}"
 
   DB_OUT_PATH="${BACKUPS_DIR}/${DB_NAME}-${DATE}.sql.gz"
-  FILES_OUT_PATH="${BACKUPS_DIR}/${DB_NAME}-files-${DATE}.zip"
 
   echo "$(date +"%Y-%m-%d %H:%M:%S") Beginning backup for $DB_OUT_PATH"
   docker compose exec db mariadb-dump --defaults-extra-file=/host/db/db_creds.cnf "$DB_NAME" | gzip -c > "$DB_OUT_PATH"
 
-  echo "$(date +"%Y-%m-%d %H:%M:%S") Beginning backup for $FILES_OUT_PATH"
+  echo "$(date +"%Y-%m-%d %H:%M:%S") Beginning AWS S3 sync for $INSTANCE_NAME files"
   docker compose exec "$INSTANCE_NAME" aws s3 sync /var/lib/humhub/ s3://mhai-humhub-mw-files-backups/"$INSTANCE_NAME"/
 
   echo "$(date +"%Y-%m-%d %H:%M:%S") Uploading to S3"
   /usr/local/bin/aws --profile mhai s3 cp "$DB_OUT_PATH" "s3://mhai-humhub-mw-backups/$INSTANCE_NAME/$(basename "$DB_OUT_PATH")"
 
   # Clean up old backups
+  echo "$(date +"%Y-%m-%d %H:%M:%S") Cleaning up old backups of ${DB_NAME}"
   ls -tp "${BACKUPS_DIR}/${DB_NAME}"*.sql.gz | grep -v '/$' | tail -n +5 | xargs -I {} rm -- {}
 done
